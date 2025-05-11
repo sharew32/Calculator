a9032880da1343ada7a93dcd33030ac014de560c
sharew32 <sharewendeshw8@gmail.com>
2025-05-11 15:48:39 -0700

    first commit

---
+++ app/src/main/java/com/sharew/calculator/MainActivity.java
@@ -0,0 +1,78 @@
+package com.sharew.calculator;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Toolbar setup
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // AdMob setup (MobileAds initialization)
+        MobileAds.initialize(this, initializationStatus -> {});
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        // WebView setup
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient()); // Ensures links open within the WebView
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Enables JavaScript for your HTML page
+        webView.loadUrl("file:///android_asset/calculator.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack(); // Go back to the previous page within the WebView
+        } else {
+            super.onBackPressed(); // If there's no page to go back to, exit the activity
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu); // Inflate the menu from XML
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_about) {
+            Toast.makeText(this, "Calculator by Sharew", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.menu_update) {
+            Toast.makeText(this, "Check Play Store for updates", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.menu_settings) {
+            Toast.makeText(this, "Settings coming soon", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
